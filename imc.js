c66a25bafd9fac9abcc3f436ff7aa7d1adc0492d
Souzanleticia <161047611+Souzanleticia@users.noreply.github.com>
2024-11-08 11:12:05 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,6 @@
+letpeso= parseFloat (prompt("informe seu peso"));
+letAltura=parseFloat(prompt("informe sua altura"));
+if(isNaN(peso)||isNaN(altura)||peso<=0||altura <=0 {
+    document.write()
+
+})
